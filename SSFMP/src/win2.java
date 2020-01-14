7abc9201f60645d778edd01d3cce33d2740dcf42
raywater110699 <57536682+raywater110699@users.noreply.github.com>
2020-01-14 14:07:08 +0800

    init
    init upload

---
+++ SSFMP/src/win2.java
@@ -0,0 +1,114 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+public class win2 extends JFrame 
+{
+	static String[] Disease_name = new String [150];
+	static String drugname[] = new String[200];
+	static String drug_ch1, drug_ch2, drug_ch3, drug_ch4, drug_ch5 = "";
+	private static final long serialVersionUID = 1L;
+	public win2(String s) 
+	 {
+		super("建議用藥組合"); 
+		setBounds(900, 200, 300, 200); 
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE); 
+		setVisible(true); 
+		Container pn = getContentPane(); 
+		try {
+			MAIN.ConnectDB();
+			Statement smt = MAIN.ConnectDB().createStatement();
+	    	ResultSet rs = smt.executeQuery("SELECT * from 工作表1 WHERE Disease ='"+s+"'");
+	    	
+	    	Statement stmt = MAIN.ConnectDB().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE); 
+	    	ResultSet rset = stmt.executeQuery("SELECT * from 工作表1 WHERE Disease ='"+s+"'"); 
+	    	ResultSetMetaData rsmd = rset.getMetaData(); 
+	    	
+	    	int columnCount = rsmd.getColumnCount();
+	    	String Name[] = new String[8];
+	    	int count = 0;
+	    	int non_null=0;
+	    	int E[]=new int[columnCount+1];
+	    	
+	    	while(rset.next()) {	    		
+	    		non_null=0;
+	    			for(int i=1; i<=columnCount; i++)
+	    			{    		
+	    				if(i!=1) {
+	    					if(rset.getInt(i) != 0) {
+	    							Name[non_null] = rsmd.getColumnName(i);
+	    							E[i]=1;
+	    							drugname[i-1] = Name[non_null];		
+	    							non_null++;
+	    					}
+	    				}
+	    				else{
+	    					drugname[0] = "Disease";
+	    					non_null++;
+	    				}
+	    			}
+	    			count++;
+	    	}		
+	    	while(count>=0 ) {
+	    		count--;
+	    		rset.previous();
+	    	}
+	    	rset.next();
+	    	for(int n=0; n<=columnCount-1; n++) {	
+	    			if(E[n]==1) {
+	    				JLabel L = new JLabel(drugname[n-1]);
+	    				pn.add(L);
+	    				//System.out.print("　,　"+drugname[n-1]);
+	    			}
+	    			else if(n==0){
+	    				JLabel L = new JLabel(drugname[0]);
+	    				pn.add(L);
+	    				//System.out.print(drugname[0]);			    				
+	    			}
+	    		}
+	    	
+	    	System.out.print("\n");
+	    	
+	    	while(rs.next()) {			    		
+	    			for(int col=1; col<=columnCount-1; col++)
+	    			{
+	    				if(col!=1) {
+	    					if(rs.getInt(col) != 0) {	    					
+	    							Disease_name[col] = rs.getString(col);
+	    							JLabel L = new JLabel(Disease_name[col]);
+	    							pn.add(L);
+	    							//System.out.print("　,　"+Disease_name[col]);	
+	    					}
+	    					else if(E[col]==1) {
+	    						JLabel L = new JLabel("0");
+	    						pn.add(L);
+	    						//System.out.print("　,　　0");
+	    					}
+	    				}
+	    				else {
+	    					JLabel L = new JLabel(rs.getString(col));
+	    					pn.add(L);
+	    					//System.out.print(rs.getString(col));
+	    				}
+	    			}		
+	    			System.out.print("\n");
+	    	 }	
+	    	
+	    	
+	    	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+	 }
+}
---
+++ SSFMP/src/win3.java
@@ -0,0 +1,97 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+public class win3 extends JFrame implements ActionListener{
+	
+	static JButton confirm = new JButton("我知道了！");
+	public win3(float predict) {
+		super("評估視窗"); 
+		setBounds(900, 200, 300, 200); 
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE); 
+		setVisible(true); 
+		setBackground(Color.white); 
+		
+		//容器 
+		Container pn = getContentPane(); 
+		pn.setLayout(new BorderLayout());
+		pn.add(confirm, BorderLayout.PAGE_END);
+		confirm.setSize(80,60);
+		//設定成FlowLayout 
+		//FlowLayout fy = new FlowLayout(); 
+		//pn.setLayout(fy); 
+		
+		confirm.addActionListener(this);
+		confirm.setBackground(Color.white); 
+		//依照predict值設定評估文字
+		
+		/*
+		if(predict>=84) {
+			JLabel l1 = new JLabel("目前用藥安全");
+			l1.setFont(new Font("標楷體",Font.BOLD,16));
+			l1.setHorizontalAlignment(SwingConstants.CENTER);
+			pn.add(l1); 
+			setContentPane(pn);				
+		}
+		
+		if(predict<84) {
+			JLabel l1 = new JLabel("目前用藥危險");
+			l1.setFont(new Font("標楷體",Font.BOLD,16));
+			l1.setHorizontalAlignment(SwingConstants.CENTER);
+			pn.add(l1); 
+			setContentPane(pn);
+		}*/
+		
+		if( predict>=0 && predict<25 ) {
+			JLabel l1 = new JLabel("目前用藥異常，請三思！！！");
+			l1.setFont(new Font("標楷體",Font.BOLD,20));
+			l1.setHorizontalAlignment(SwingConstants.CENTER);
+			pn.add(l1); 
+			setContentPane(pn);				
+		}
+		
+		if( predict>=25 && predict<50) {
+			JLabel l1 = new JLabel("目前用藥在合理範圍內，但存在一定風險！！");
+			l1.setFont(new Font("標楷體",Font.BOLD,20));
+			l1.setHorizontalAlignment(SwingConstants.CENTER);
+			pn.add(l1); 
+			setContentPane(pn);				
+		}
+		
+		if(predict>=50 && predict<75) {
+			JLabel l1 = new JLabel("目前用藥略微異常，請再確認！");
+			l1.setFont(new Font("標楷體",Font.BOLD,20));
+			l1.setHorizontalAlignment(SwingConstants.CENTER);
+			pn.add(l1); 
+			setContentPane(pn);				
+		}
+		
+		if(predict>=75) {
+			JLabel l1 = new JLabel("目前用藥在合理範圍內～");
+			l1.setFont(new Font("標楷體",Font.BOLD,20));
+			l1.setHorizontalAlignment(SwingConstants.CENTER);
+			pn.add(l1); 
+			setContentPane(pn);				
+		}
+		
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {	// "我知道了！"的觸發
+		if((JButton) e.getSource()== confirm)
+		{
+			dispose();
+		}
+	}
+	
+}
